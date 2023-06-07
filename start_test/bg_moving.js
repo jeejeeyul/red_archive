d36da0ed2084b2295714a31f228d4ee0d4c90ce8
jeejeeyul <jee200713@gmail.com>
2023-06-07 15:58:55 +0900

    오프닝만 만들면 기초는 끝

--- start_test/bg_moving.js
+++ start_test/bg_moving.js
@@ -1,8 +1,13 @@
 var bg_img = document.getElementById("bg_img");
+var blocker = document.getElementById("blocker");
 console.log(bg_img);
 bg_img.addEventListener("click", function () {
+  blocker.style.zIndex = 1000;
   bg_img.classList.add("slidein");
   setTimeout(() => {
     bg_img.classList.remove("slidein");
+    blocker.classList.remove("block");
+    blocker.style.zIndex = -1000;
   }, 3000);
 });
+//끝
