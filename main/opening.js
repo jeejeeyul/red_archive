32da89498dc4a205ccc418b86bdb507d35a153a6
jeejeeyul <jee200713@gmail.com>
2023-06-10 18:13:50 +0900

    임시저장

--- main/opening.js
+++ main/opening.js
@@ -1,13 +1,27 @@
 var btn = document.getElementById("btn");
 var text_box = document.getElementsByClassName("text_box");
 var text = document.getElementsByClassName("text");
+var sel_con = document.getElementById("sel_con");
 
-var scription = `고향에 가던 중 휴게소에 들렸지만 브레이크가 고장이 나 의류매장 박아버리고 말았다. 
+var scription = `고향에 가던 중 휴게소에 들렸지만 브레이크가 고장이 나 의류매장에 박아버리고 말았다.   
   버스가 수리될 때까지 시간이 걸려 옷을 둘러보기로 했다.`;
+var i = 0;
 btn.addEventListener("click", function () {
   btn.style.visibility = "hidden";
   text_box[0].style.zIndex = "1000";
-  for (let i = 0; i < scription.length; i++) {
-    console.log(scription[i]); //배열로 뽑아서 setInterval로 어쩌구 저쩌구 미래의 나 알아서 잘 해봐라
-  }
+
+  /* console.log(scription[i]);*/
+  var interval = setInterval(() => {
+    text[0].append(scription[i]);
+    console.log(scription[i]);
+    i++;
+    if (i == scription.length) {
+      clearInterval(interval);
+    }
+  }, 100);
+
+  setTimeout(() => {
+    text[0].textContent = "";
+    sel_con.style.visibility = "visible";
+  }, 11000);
 });
